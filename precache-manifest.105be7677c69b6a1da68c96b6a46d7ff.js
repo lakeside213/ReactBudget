98405d96efad1e87e23389283aba07319ebcf846
lekan <kokulekan23@gmail.com>
2019-04-24 10:37:29 +0200

    Updates

---
+++ precache-manifest.105be7677c69b6a1da68c96b6a46d7ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8b582cecddca36a006107b0956c56c",
+    "url": "/ReactBudget/index.html"
+  },
+  {
+    "revision": "ef0b885b4c2139345d95",
+    "url": "/ReactBudget/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ReactBudget/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ef0b885b4c2139345d95",
+    "url": "/ReactBudget/static/js/main.33f30726.chunk.js"
+  },
+  {
+    "revision": "c55655acc55370ac3a2b",
+    "url": "/ReactBudget/static/js/runtime~main.38b7b471.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactBudget/static/media/logo.5d5d9eef.svg"
+  }
+]);
