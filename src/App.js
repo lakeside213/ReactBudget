4fea6b4a17f5e7a3ec2756bca092f6a4589c9c7a
lekan <kokulekan23@gmail.com>
2019-04-26 00:13:32 +0200

    added Fab

--- src/App.js
+++ src/App.js
@@ -2,29 +2,6 @@ import React, { Fragment, Component } from "react";
 import Header from "./components/Header";
 import BottomNav from "./components/BottomNav";
 import HomePage from "./components/Home";
-import Home from "@material-ui/icons/Home";
-import AccountBalance from "@material-ui/icons/AccountBalance";
-import Person from "@material-ui/icons/Person";
-import Money from "@material-ui/icons/AttachMoney";
-
-const pages = [
-    {
-        name: "Home",
-        icon: <Home />
-    },
-    {
-        name: "Accounts",
-        icon: <AccountBalance />
-    },
-    {
-        name: "Budget",
-        icon: <Money />
-    },
-    {
-        name: "Me",
-        icon: <Person />
-    }
-];
 
 class App extends Component {
     state = {
@@ -40,11 +17,7 @@ class App extends Component {
             <Fragment>
                 <Header pageName={selectedPage} />
                 <HomePage />
-                <BottomNav
-                    pages={pages}
-                    setPage={this.setPage}
-                    value={selectedPage}
-                />
+                <BottomNav setPage={this.setPage} value={selectedPage} />
             </Fragment>
         );
     }
--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,19 +3,33 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import Home from "@material-ui/icons/Home";
+import AccountBalance from "@material-ui/icons/AccountBalance";
+import Person from "@material-ui/icons/Person";
+import Money from "@material-ui/icons/AttachMoney";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 
-const styles = {
+const styles = theme => ({
     root: {
         width: "100%",
         position: "fixed",
         bottom: 0,
         marginTop: "8%"
+    },
+    fab: {
+        marginTop: "-1.25rem"
+    },
+    actionWidth: {
+        [theme.breakpoints.down("xs")]: {
+            minWidth: "65px"
+        }
     }
-};
+});
 
 class BottomNav extends React.Component {
     render() {
-        const { classes, pages, setPage, value } = this.props;
+        const { classes, setPage, value } = this.props;
 
         return (
             <BottomNavigation
@@ -23,14 +37,43 @@ class BottomNav extends React.Component {
                 onChange={setPage}
                 className={classes.root}
             >
-                {pages.map((page, index) => (
-                    <BottomNavigationAction
-                        label={page.name}
-                        value={page.name}
-                        icon={page.icon}
-                        key={index}
-                    />
-                ))}
+                <BottomNavigationAction
+                    label="Home"
+                    value="Home"
+                    icon={<Home />}
+                    className={classes.actionWidth}
+                />
+                <BottomNavigationAction
+                    label="Accounts"
+                    value="Accounts"
+                    icon={<AccountBalance />}
+                    className={classes.actionWidth}
+                />
+                <BottomNavigationAction
+                    className={classes.fab}
+                    icon={
+                        <Fab
+                            color="primary"
+                            aria-label="Add"
+                            className={classes.fab}
+                        >
+                            <AddIcon />
+                        </Fab>
+                    }
+                    className={classes.actionWidth}
+                />
+                <BottomNavigationAction
+                    label="Budget"
+                    value="Budget"
+                    icon={<Money />}
+                    className={classes.actionWidth}
+                />
+                <BottomNavigationAction
+                    label="Me"
+                    value="Me"
+                    icon={<Person />}
+                    className={classes.actionWidth}
+                />
             </BottomNavigation>
         );
     }
