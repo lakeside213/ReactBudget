357c72b48267a268dc302006295234a231d264ec
lekan <kokulekan23@gmail.com>
2019-04-28 18:35:34 +0200

    bug fixes

--- src/components/Profile/selectCurrency.js
+++ src/components/Profile/selectCurrency.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
+
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
@@ -11,8 +12,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/ChevronLeft";
-import logo from "../../logo.svg";
 import countries from "../../utils/countries.json";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 const styles = theme => ({
     root: {
         width: "100%"
@@ -35,66 +36,79 @@ const styles = theme => ({
     }
 });
 
-function SimpleList(props) {
-    const { classes, setPage, dialogToggle } = props;
-    return (
-        <div className={classes.root}>
-            <AppBar className={classes.appBar}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="Close"
-                        onClick={() => {
-                            dialogToggle();
-                        }}
-                    >
-                        <CloseIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        color="inherit"
-                        className={classes.flex}
-                    >
-                        Pick your currency
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <List component="div" dense>
-                {countries.map(country => (
-                    <Fragment>
-                        <ListItem
-                            button
-                            className={classes.listItem}
-                            onClick={e => {
-                                setPage(e, "Me");
+const Flag = ({ src }) => (
+    <div>
+        <LazyLoadImage alt={"l"} height={"50px"} src={src} width={"50px"} />
+    </div>
+);
+class SimpleList extends Component {
+    state = {
+        countriesData: []
+    };
+    componentDidMount() {
+        this.setState({ countriesData: countries });
+    }
+
+    render() {
+        const { countriesData } = this.state;
+        const { classes, setPage, dialogToggle } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Close"
+                            onClick={() => {
+                                dialogToggle();
                             }}
                         >
-                            <ListItemIcon>
-                                <img
-                                    src={country.flag}
-                                    className={classes.flag}
-                                />
-                            </ListItemIcon>
+                            <CloseIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            color="inherit"
+                            className={classes.flex}
+                        >
+                            Pick your currency
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <List component="div" dense>
+                    {countriesData.map(country => (
+                        <Fragment key={country.name}>
+                            <ListItem
+                                button
+                                className={classes.listItem}
+                                onClick={e => {
+                                    dialogToggle();
+                                    setPage(e, "Me");
+                                }}
+                            >
+                                <ListItemIcon>
+                                    <Flag src={country.flag} />
+                                </ListItemIcon>
 
-                            <ListItemText
-                                primary={
-                                    <Typography variant="h6">
-                                        {`${country.name}(${
-                                            country.currencies[0].name
-                                        })`}
-                                    </Typography>
-                                }
-                                secondary={`${country.currencies[0].code}(${
-                                    country.currencies[0].symbol
-                                })`}
-                            />
-                        </ListItem>
-                        <Divider />
-                    </Fragment>
-                ))}
-            </List>
-        </div>
-    );
+                                <ListItemText
+                                    primary={
+                                        <Typography variant="h6">
+                                            {`${country.name}(${
+                                                country.currencies[0].name
+                                            })`}
+                                        </Typography>
+                                    }
+                                    secondary={`${country.currencies[0].code}(${
+                                        country.currencies[0].symbol
+                                    })`}
+                                />
+                            </ListItem>
+                            <Divider />
+                        </Fragment>
+                    ))}
+                </List>
+            </div>
+        );
+    }
 }
 
 SimpleList.propTypes = {
